e3a00915562efe8b64eed32bcaa873be9bf0ab64
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-03-11 23:16:39 +0000

    index add

---
+++ resources/views/todo/create.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ToDo List</title>
+    
+    <!-- Styles -->
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/todo.css') }}" rel="stylesheet">
+  </head>
+  <body>
+    <dib class="row">
+      <h1 class="pl-5 pt-2">ToDo List</h1>
+    </dib>
+    
+  </body>
+</html>
